1ab6b0f51148cb7a9dcae6c6a61300a0d2383aca
Barbara <barbi.medica@gmail.com>
2026-06-10 21:03:27 +0200

    Initial commit

---
+++ src/test/java/hr/example/farmtrack/FarmTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.example.farmtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
