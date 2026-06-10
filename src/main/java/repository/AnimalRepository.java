6c413d81093d57d22dd0a101858bd4779d2f9e58
Barbara <barbi.medica@gmail.com>
2026-06-10 21:49:57 +0200

    Stvorena struktura projekta i dodani AnimalRepository i Animal entity

---
+++ src/main/java/repository/AnimalRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import entity.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+
+    List<Animal> findByImeContainingIgnoreCase(String ime);
+}
